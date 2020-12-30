8c2fdcf3d8e46b07446cfebd1eec497c40d17673
Augusto Pascutti <augusto.hp@gmail.com>
2020-12-30 14:18:26 -0300

    Prefer tabs instead of spaces

--- template.sh
+++ template.sh
@@ -9,86 +9,91 @@ cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 
 trap cleanup SIGINT SIGTERM ERR EXIT
 
-usage() {
-  cat <<EOF
-Usage: $(basename "$0") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+# Usage: display_help_and_exit
+display_help_and_exit() {
+	cat <<-EOF
+	Usage: $(basename "$0") [-h] [-v] [-f] -p param_value arg1 [arg2...]
 
-Script description here.
+	Script description here.
 
-Available options:
+	Available options:
 
--h, --help      Print this help and exit
--v, --verbose   Print script debug info
--f, --flag      Some flag description
--p, --param     Some param description
+	-h, --help      Print this help and exit
+	-v, --verbose   Print script debug info
+	-f, --flag      Some flag description
+	-p, --param     Some param description
 EOF
-  exit
+	exit 1
 }
 
+# Usage: cleanup
 cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-  # script cleanup here
+	trap - SIGINT SIGTERM ERR EXIT
 }
 
+# Usage: setup_colors
 setup_colors() {
-  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-    NOCOLOR='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-  else
-    NOCOLOR='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-  fi
+	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+		NOCOLOR='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+	else
+		NOCOLOR='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+	fi
 }
 
+# Usage: msg <message>
 msg() {
-  echo >&2 -e "${1-}"
+	echo >&2 -e "${1-}"
 }
 
+# Usage: die <message> [exit code]
 die() {
-  local msg=$1
-  local code=${2-1} # default exit status 1
-  msg "$msg"
-  exit "$code"
+	local msg=$1
+	local code=${2-1} # default exit status 1
+	msg "$msg"
+	exit "$code"
 }
 
+# Usage: parse_params
 parse_params() {
-  # default values of variables set from params
-  flag=0
-  param=''
+	# default values of variables set from params
+	flag=0
+	param=''
 
-  while :; do
-    case "${1-}" in
-    -h | --help)
-      usage
-      ;;
-    -v | --verbose)
-      set -x
-      ;;
-    --no-color)
-      NO_COLOR=1
-      ;;
-    -f | --flag) # example flag
-      flag=1
-      ;;
-    -p | --param) # example named parameter
-      param="${2-}"
-      shift
-      ;;
-    -?*)
-      die "Unknown option: $1"
-      ;;
-    *)
-      break
-      ;;
-    esac
-    shift
-  done
+	while :; do
+		case "${1-}" in
+		-h | --help)
+			display_help_and_exit
+			;;
+		-v | --verbose)
+			set -x
+			;;
+		--no-color)
+			NO_COLOR=1
+			;;
+		-f | --flag) # example flag
+			flag=1
+			;;
+		-p | --param) # example named parameter
+			param="${2-}"
+			shift
+			;;
+		-?*)
+			die "Unknown option: $1"
+			;;
+		*)
+			break
+			;;
+		esac
+		shift
+	done
 
-  args=("$@")
+	args=("$@")
 
-  # check required params and arguments
-  [[ -z "${param-}" ]] && die "Missing required parameter: param"
-  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
+	# check required params and arguments
+	[[ -z "${param-}" ]] && die "Missing required parameter: param"
+	[[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
 
-  return 0
+	return 0
 }
 
 parse_params "$@"
